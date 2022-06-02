87ab99844f88f468d189763c1b633b9bcdf4f5a9
ayush gupta <ayush.gupta_cs19@gla.ac.in>
2022-06-02 20:20:18 +0530

    added Home page component and displayed list of songs with mock data

---
+++ app-frontend/src/components/Home.page.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+export default function HomePage() {
+  const data = {
+    headings: ["Artwork", "Song", "Date of Release", "Artist", "Rate"],
+    artists: [{
+        name:'Justin Bieber',
+        song:'Sorry',
+        date:'5th sept 2022',
+        image:'hi',
+
+    },
+    {
+        name:'Justin Bieber',
+        song:'Sorry',
+        date:'5th sept 2022',
+        image:'hi',
+
+    },
+    {
+        name:'Justin Bieber',
+        song:'Sorry',
+        date:'5th sept 2022',
+        image:'hi',
+
+    },
+    {
+        name:'Justin Bieber',
+        song:'Sorry',
+        date:'5th sept 2022',
+        image:'hi',
+
+    },
+    
+],
+  };
+  const headings = data.headings;
+  const artists = data.artists;
+  return (
+    <div>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            {
+                headings.map((heading) => {
+                    return <th scope="col">{heading}</th>
+                })
+            }  
+            {/* <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {artists.map((artist,index) => {
+              return <tr>
+                  <th>artwork</th>
+                  <th>{artist.song}</th>
+                  <th>{artist.date}</th>
+                  <th>{artist.name}</th>
+                  <th>rate</th>
+              </tr>
+          })}  
+          {/* <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr> */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
